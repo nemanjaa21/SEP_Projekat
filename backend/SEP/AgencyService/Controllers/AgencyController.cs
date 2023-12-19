a6f01e7843eab352a624cd5fff5bc353850826bf
Dejan Kurdulija <101801385+KurdulijaD@users.noreply.github.com>
2023-12-19 17:47:21 +0100

    ..

--- backend/SEP/AgencyService/Controllers/AgencyController.cs
+++ backend/SEP/AgencyService/Controllers/AgencyController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AgencyService.DTO;
+using AgencyService.Interfaces;
+using AgencyService.Service;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgencyService.Controllers
@@ -7,5 +11,24 @@ namespace AgencyService.Controllers
     [ApiController]
     public class AgencyController : ControllerBase
     {
+        private readonly IAgencyService _agencyService;
+        private readonly IMapper _mapper;
+
+        public AgencyController(IAgencyService agencyService, IMapper mapper)
+        {
+            _agencyService = agencyService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAgencyById(int id)
+        {
+            var agency = await _agencyService.GetAgencyById(id);
+            if (agency == null)
+            {
+                return NotFound($"Agency with id {id} does not exist!");
+            }
+            return Ok(_mapper.Map<AgencyDto>(agency));
+        }
     }
 }
